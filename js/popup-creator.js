5ce4c1018fe4f9bc3d7057d16e67537f372c9cb3
Ilya Shibaev <hausdr@gmail.com>
2022-06-21 00:26:45 +0300

    fix

--- js/popup-creator.js
+++ js/popup-creator.js
@@ -5,11 +5,11 @@ const commentTemplate = commentsList.querySelector('.social__comment');
 const createPopup = (card) => {
 
   const cardData = card;
+  const postComments = cardData.comments;
   const closeButton = popupTemplate.querySelector('#picture-cancel');
-  const commentsPerPage = 5;
   const buttonShowMore = popupTemplate.querySelector('.comments-loader');
   const showedCommentsCount = popupTemplate.querySelector('.showed-comments-count');
-  const postComments = cardData.comments;
+  const commentsPerPage = 5;
   let showedCommentAmount = commentsPerPage;
 
   const openPopup = () => {
@@ -27,33 +27,27 @@ const createPopup = (card) => {
         closePopup();
       }
   };
+  const renderComments = (commentsToRender) => {
+    let commentsFragment = document.createDocumentFragment();
+
+    commentsToRender.forEach( dataComment => {
+      let comment = commentTemplate.cloneNode(true);
+      const img = comment.querySelector('.social__picture');
+      img.src = dataComment.avatar;
+      img.alt = dataComment.name;
+      comment.querySelector('.social__text').textContent = dataComment.message;
+      commentsFragment.append(comment);
+    });
+    commentsList.innerHTML = '';
+    commentsList.append(commentsFragment);
+  };
 
   popupTemplate.querySelector('.big-picture__img img').src = cardData.url;
   popupTemplate.querySelector('.likes-count').textContent = cardData.likes;
   popupTemplate.querySelector('.comments-count').textContent = `${cardData.comments.length}`;
   popupTemplate.querySelector('.social__caption').textContent = cardData.description;
 
-  //комментарии
-
-  //const commentCountElement = popupTemplate.querySelector('.social__comment-count');
-
   let commentsToRender = postComments.slice(0, commentsPerPage);
-
-  const renderComments = (commentsToRender) => {
-    let commentsFragment = document.createDocumentFragment();
-
-    commentsToRender.forEach( dataComment => {
-    let comment = commentTemplate.cloneNode(true);
-    const img = comment.querySelector('.social__picture');
-    img.src = dataComment.avatar;
-    img.alt = dataComment.name;
-    comment.querySelector('.social__text').textContent = dataComment.message;
-    commentsFragment.append(comment);
-  });
-  commentsList.innerHTML = '';
-  commentsList.append(commentsFragment);
-  };
-
   renderComments(commentsToRender);
 
   const clickShowMore = (evt) => {
